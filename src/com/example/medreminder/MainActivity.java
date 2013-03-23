8a715207ba683a266a2089bc0c8ef226fabbc06f
Hongyu Chen <hychenisu@gmail.com>
2013-03-23 00:18:38 -0400

    able to custermize tab

--- src/com/example/medreminder/MainActivity.java
+++ src/com/example/medreminder/MainActivity.java
@@ -2,33 +2,16 @@ package com.example.medreminder;
 
 import java.util.Locale;
 
-
-
 import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.app.FragmentTransaction;
-import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.ListFragment;
-import android.support.v4.app.NavUtils;
 import android.support.v4.view.ViewPager;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TabHost;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends FragmentActivity implements
 		ActionBar.TabListener {
@@ -88,6 +71,7 @@ public class MainActivity extends FragmentActivity implements
 		//TODO::change the tab color to gray
 		//actionBar.setStackedBackgroundDrawable(getResources().getDrawable(
 	    //        R.drawable.ic_launcher));
+		
 		/*
 		// For each of the sections in the app, add a tab to the action bar.
 		for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
@@ -172,6 +156,10 @@ public class MainActivity extends FragmentActivity implements
 				return fragment2;
 			}
 			else if(position == 3){
+				Fragment fragment3 = new settingActivity();
+				return fragment3;
+			}
+			else if(position == 4){
 				Fragment fragment3 = new settingActivity();
 				return fragment3;
 			}
